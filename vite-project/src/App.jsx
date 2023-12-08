84013a89a7d6b7e98ea54988fd86207704df6360
karunakar <karunakaryadav167@gmail.com>
2023-12-08 11:35:51 +0530

    Initial commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import Navbar from './Components/Nav.jsx';
+import AllRoutes from './Routes/AllRoutes';
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <AllRoutes/>
+    </>
+  )
+}
+
+export default App
